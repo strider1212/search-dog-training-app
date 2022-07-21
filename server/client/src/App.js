4af8ca37e16cd172279d6f0a64b63a33fba466f9
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-21 17:50:34 -0400

    test added text in /client to sure that it all commits under the same .git

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing 1, 2, 3
         </a>
       </header>
     </div>
