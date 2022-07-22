cc2b7a16197bf033b7176996a7f37b0c43057994
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-22 17:11:28 -0400

    GET / test works and listens on localhost 3000

--- server/server.js
+++ server/server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Testing 1, 2, 2...')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`)
+})
