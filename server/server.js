8eb646d62a710bc4df1759ecc32fbc4fc47df21c
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-22 18:05:18 -0400

    added comment on express.Router()

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 3000;
 
 //use app.route() to chaine routes. See https://expressjs.com/en/guide/routing.html
+//make modular with express.Router(). ibid.
 
 app.get('/', (req, res) => {
   res.send('Hello World! Testing 1, 2, 2...')
