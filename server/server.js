06ba43949bab6c9ef2a59305c047f15baa4c67e2
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-25 09:36:30 -0400

    I deleted the routes because they do not make sense without the database

--- server/server.js
+++ server/server.js
@@ -5,16 +5,6 @@ const PORT = 3000;
 
 app.use(express.json())
 
-app.route('/logs')
-  .get((req, res) => {
-    res.send('test')
-  })
-
-app.route('/logs/:id')
-  .get((req, res) => {
-    const id = req.params.id;
-    res.send(id)
-  })
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`)
