41c717c032f64f7d9b47094fc1cd824bafb7aab6
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-25 09:31:47 -0400

    /logs GET works

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ app.route('/logs')
     res.send('test')
   })
 
+app.route('/logs/:id')
+  .get((req, res) => {
+    const id = req.params.id;
+    res.send(id)
+  })
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`)
 })
