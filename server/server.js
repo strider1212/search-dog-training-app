4ad9d26b95b3df58a76b6d471468c2e5990d7c7e
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-26 08:44:34 -0400

    created userSchema

--- server/server.js
+++ server/server.js
@@ -8,6 +8,56 @@ const PORT = 3000;
 
 app.use(express.json())
 
+//create a schema
+//create a model
+//instantiate model inside of application
+//impliment in post function using the model
+  //get to work first
+  //then get to work with user input
+//test
+//make modular
+
+
+const userSchema = new mongoose.Schema({
+  "username": {
+    type: String,
+    required: true
+  },
+  "password": {
+    type: String,
+    required: true
+  },
+  "firstName": {
+    type: String,
+    required: true
+  },
+  "lastName": {
+    type: String,
+    required: true
+  },
+  "email": {
+    type: String,
+    required: true
+  },
+  "phone_number": {
+    type: Number,
+    required: true
+  },
+  "dateCreated": {
+    type: Date,
+    required: true
+  },
+  "k9s": {
+    type: Array,
+    required: true
+  },
+})
+
+app.route('/users/:id')
+  .post((res, req) => {
+
+  })
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`)
