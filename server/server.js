b5b01ca60edc637904edf1c012b537f50be5177f
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-27 10:38:55 -0400

    added descriptive comments for developers

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
+//general requirements
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config({path: '../.env'});
 
+//Mongoose requirements
 const { User } = require('./mongoose/user');
 
+//.env imports
 const connectUsername = process.env.USERNAME;
 const connectPassword = process.env.PASSWORD;
 const connectDatabase = process.env.DATABASE;
 
+//staging variables
 const ATLAS_CONNECT = `mongodb+srv://${connectUsername}:${connectPassword}@cluster0.tgm5d.mongodb.net/${connectDatabase}`;
 const PORT = 3000;
 
 mongoose.connect(ATLAS_CONNECT);
 
+//middleware
 app.use(express.json())
 
+//routes and methods
 app.route('/users')
   .post(async (req, res) =>  {
     //check all field on the front end
@@ -48,5 +54,4 @@ app.listen(PORT, () => {
 })
 
 
-//refactor POST /users/:id to be for /users
 //handle error
