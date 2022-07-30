9aa72fb0fb096d576ecb7b5bce99fdf1f2a3c340
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-30 11:58:41 -0400

    filled out teamSchema

--- server/mongoose/team.js
+++ server/mongoose/team.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
+  "team_name": String,
+  "created_by": {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  "date_created": new Date(),
+  "members": Array,
+  "admin_members": Array
 })
 
 const Team = mongoose.model('Team', teamSchema);
