6aafd17a1341ab992e02e47ff5088d39a7cd9091
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-30 12:17:19 -0400

    updated teamSchema so that members and admin_members are arrays of objectIds

--- server/mongoose/team.js
+++ server/mongoose/team.js
@@ -4,8 +4,8 @@ const teamSchema = new mongoose.Schema({
   "team_name": String,
   "created_by": {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   "date_created": Date,
-  "members": Array,
-  "admin_members": Array
+  "members": [mongoose.Types.ObjectId],
+  "admin_members": [mongoose.Types.ObjectId]
 })
 
 const Team = mongoose.model('Team', teamSchema);
