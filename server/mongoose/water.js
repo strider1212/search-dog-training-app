a02e42734dd173018b188ef50c46058827fc1c81
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-30 13:56:21 -0400

    filled in waterSchema

--- server/mongoose/water.js
+++ server/mongoose/water.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-const waterSchema = new mongoose.Schema({})
+const waterSchema = new mongoose.Schema({
+  "open": Boolean,
+  "submerged": Boolean,
+  "depth": Number,
+  "salt_water": Boolean,
+  "water_type": String,
+  "temperature": Number
+})
 
 const Water = mongoose.model('Water', waterSchema);
 
