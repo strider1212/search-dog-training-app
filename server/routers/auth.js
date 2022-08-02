af411299ac7a060bb6a58ffa87b549fb06b0d43e
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-02 15:40:17 -0400

    cleared out LocalStrategy to re-think it

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -10,13 +10,7 @@ app.use(passport.session());
 passport.use(
   "local",
   new LocalStrategy((username, password, done) => {
-    const authenticated = username === "John" && password === "Smith";
-
-    if (authenticated) {
-      return done(null, { myUser: "user", myID: 1234 });
-    } else {
-      return done(null, false);
-    }
+    
   })
 );
 
