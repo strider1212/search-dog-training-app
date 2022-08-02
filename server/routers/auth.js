b8af990c960213b881f7b4caaf0621ed27d0e5f7
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-02 12:17:26 -0400

    refactored auth.js

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,6 +1,7 @@
-var express = require('express');
+const express = require('express');
 
-var router = express.Router();
+
+const router = express.Router();
 
 router.get('/login', function(req, res, next) {
   console.log('connected')
