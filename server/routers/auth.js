175ea6af4a16399aa4106b37ce860ff790fe81f3
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-02 15:58:46 -0400

    created a setup of LocalStrategy

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const { User } = require('../mongoose/user');
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -10,7 +12,15 @@ app.use(passport.session());
 passport.use(
   "local",
   new LocalStrategy((username, password, done) => {
-    
+    User.findOne({username: username}, (err, user) => {
+      if (err) return done(err);
+
+      if (!user) return done(null, false);
+
+      if (user.password != password) return done(null, false);
+
+      return done(null, user);
+    })
   })
 );
 
