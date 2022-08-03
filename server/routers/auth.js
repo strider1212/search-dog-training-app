40a38139836c69d03ed4e0d239678a6c50bf8bce
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-03 16:35:31 -0400

    adjusted the link to the .env after moving it

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,65 +1,65 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+// const express = require('express');
+// const router = express.Router();
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+// const session = require('express-session');
 
-const { User } = require('../mongoose/user');
-const compareHash = require('../utils/compareHash');
+// const { User } = require('../mongoose/user');
+// const compareHash = require('../utils/compareHash');
 
 
-router.use(passport.initialize());
-router.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}))
-router.use(passport.session());
+// router.use(passport.initialize());
+// router.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { secure: true }
+// }))
+// router.use(passport.session());
 
-passport.use(
-  "local",
-  new LocalStrategy((username, password, done) => {
-    User.findOne({username: username}, (err, user) => {
-      if (err) return done(err);
+// passport.use(
+//   "local",
+//   new LocalStrategy((username, password, done) => {
+//     User.findOne({username: username}, (err, user) => {
+//       if (err) return done(err);
 
-      if (!user) return done(null, false);
+//       if (!user) return done(null, false);
 
-      const hasPassword = compareHash(password, user.password);
+//       const hasPassword = compareHash(password, user.password);
 
-      if (!hasPassword) return done(null, false);
+//       if (!hasPassword) return done(null, false);
 
-      return done(null, user);
-    })
-  })
-);
+//       return done(null, user);
+//     })
+//   })
+// );
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-})
+// passport.serializeUser((user, done) => {
+//   done(null, user._id);
+// })
 
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    if (err) return done(err);
+// passport.deserializeUser((id, done) => {
+//   User.findById(id, (err, user) => {
+//     if (err) return done(err);
 
-    done(null, user);
-  })
-})
+//     done(null, user);
+//   })
+// })
 
-router.get('/login', function(req, res, next) {
-  console.log('connected')
-  res.end()
-});
+// router.get('/login', function(req, res, next) {
+//   console.log('connected')
+//   res.end()
+// });
 
-router.post('/login/password', 
-  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-  function(req, res) {
-    res.redirect('/');
-  }
-  );
+// router.post('/login/password', 
+//   passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+//   function(req, res) {
+//     res.redirect('/');
+//   }
+//   );
 
-//create a logout using req.logout (https://www.codecademy.com/courses/user-authentication-authorization-express/lessons/passport-js-local-authentication/exercises/logging-out)
+// //create a logout using req.logout (https://www.codecademy.com/courses/user-authentication-authorization-express/lessons/passport-js-local-authentication/exercises/logging-out)
 
-//bcrypt (https://www.codecademy.com/courses/user-authentication-authorization-express/lessons/bcrypt/exercises/using-bcrypt-to-hash-passwords)
+// //bcrypt (https://www.codecademy.com/courses/user-authentication-authorization-express/lessons/bcrypt/exercises/using-bcrypt-to-hash-passwords)
 
-module.exports = router;
+// module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({path: './.env'});
 
 
 //routers
 const users = require('./routers/users');
 const teams = require('./routers/teams')
 const logs = require('./routers/logs');
-const auth = require('./routers/auth');
+// const auth = require('./routers/auth');
 
 //.env imports
 const connectUsername = process.env.USERNAME;
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use('/users', users);
 app.use('/teams', teams);
 app.use('/logs', logs);
-app.use('/auth', auth);
+// app.use('/auth', auth);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`)
