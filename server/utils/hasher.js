e5e316ca77c9e8483d7cb04f6bf9041699846a33
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-03 09:54:56 -0400

    created and exported hasher

--- server/utils/hasher.js
+++ server/utils/hasher.js
@@ -1,1 +1,13 @@
 const bcrypt = require('bcrypt');
+
+const hasher = async (password, saltRounds) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt);
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+};
+
+module.exports = hasher;
