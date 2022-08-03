ff83bc9c9d69c579e1b900fda5b16538bc30e5ee
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-03 10:35:33 -0400

    finished and exported compareHash.js

--- server/utils/compareHash.js
+++ server/utils/compareHash.js
@@ -1,1 +1,13 @@
 const bcrypt = require('bcrypt');
+
+const compareHash = async (password, hash) => {
+  try {
+    const matchFound = await bcrypt.compare(password, hash);
+    return matchFound;
+  } catch (err) {
+    console.log(err);
+  }
+  return false;
+};
+
+module.exports = compareHash;
