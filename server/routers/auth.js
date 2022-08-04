b2ce3ccfcf23d0bd2e5837043d6671f689d5559a
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-04 13:14:52 -0400

    attempting to get auth.js running

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,65 +1,50 @@
-// const express = require('express');
-// const router = express.Router();
-// const passport = require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
-// const session = require('express-session');
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const cookieSession = require('cookie-session');
 
-// const { User } = require('../mongoose/user');
-// const compareHash = require('../utils/compareHash');
+const { User } = require('../mongoose/user');
+
+router.use(passport.initialize());
 
 
-// router.use(passport.initialize());
-// router.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }))
-// router.use(passport.session());
+router.use(
+  cookieSession({
+    name: "session",
+    keys: ["helloworld"],
 
-// passport.use(
-//   "local",
-//   new LocalStrategy((username, password, done) => {
-//     User.findOne({username: username}, (err, user) => {
-//       if (err) return done(err);
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
-//       if (!user) return done(null, false);
+router.use('local', new LocalStrategy(function(username, password, done) {
+  User.findOne({username: username}, (err, user) => {
+    if (err) return done(err);
 
-//       const hasPassword = compareHash(password, user.password);
+    if (!user) return done(null, false);
 
-//       if (!hasPassword) return done(null, false);
+    if (user.password != password) return done(null, false);
 
-//       return done(null, user);
-//     })
-//   })
-// );
+    return done(null, user);
+  });
+}));
 
-// passport.serializeUser((user, done) => {
-//   done(null, user._id);
-// })
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
 
-// passport.deserializeUser((id, done) => {
-//   User.findById(id, (err, user) => {
-//     if (err) return done(err);
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    if (err) return done(err);
+    return done(null, user);
+  });
+});
 
-//     done(null, user);
-//   })
-// })
+router.post('/login', passport.authenticate('local', (req, res) => {
+  console.log('Boom!');
+  res.end();
+}))
 
-// router.get('/login', function(req, res, next) {
-//   console.log('connected')
-//   res.end()
-// });
-
-// router.post('/login/password', 
-//   passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-//   function(req, res) {
-//     res.redirect('/');
-//   }
-//   );
-
-// //create a logout using req.logout (https://www.codecademy.com/courses/user-authentication-authorization-express/lessons/passport-js-local-authentication/exercises/logging-out)
-
-// //bcrypt (https://www.codecademy.com/courses/user-authentication-authorization-express/lessons/bcrypt/exercises/using-bcrypt-to-hash-passwords)
-
-// module.exports = router;
+module.exports = router;
