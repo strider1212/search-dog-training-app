267ad5e4122a64e0a5d178ff6e476a849d9db790
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-04 13:54:28 -0400

    needs a lot of work, but I am committing it to not lose it

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -2,24 +2,17 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
-const cookieSession = require('cookie-session');
+const session = require('express-session');
 
 const { User } = require('../mongoose/user');
 
+
+router.use(session({secret: 'secrete cat'}));
+
 router.use(passport.initialize());
+router.use(passport.session());
 
-
-router.use(
-  cookieSession({
-    name: "session",
-    keys: ["helloworld"],
-
-    // Cookie Options
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-  })
-);
-
-router.use('local', new LocalStrategy(function(username, password, done) {
+passport.use('local', new LocalStrategy(function(username, password, done) {
   User.findOne({username: username}, (err, user) => {
     if (err) return done(err);
 
@@ -42,9 +35,16 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-router.post('/login', passport.authenticate('local', (req, res) => {
-  console.log('Boom!');
+router.post('/login', passport.authenticate('local', {successRedirect: '/profile', failureRedirect: '/signin'}))
+
+router.get('/profile', (req, res) => {
+  console.log('redirected!');
+  res.send("Hey, hello from the server!");
+})
+
+router.get('/signin', (req, res) => {
+  console.log('gotta sign in');
   res.end();
-}))
+})
 
 module.exports = router;
