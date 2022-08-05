115f8a2c5fa768092ab01fd3b57c894de689c463
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-05 15:46:45 -0400

    populated authUser

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -5,6 +5,8 @@ const session = require('express-session')
 const LocalStrategy = require('passport-local').Strategy
 require('dotenv').config({path: '/Users/joshuacushing/code/Parsity/final-project/search-dog-app/server/.env'});
 
+const { User } = require('../mongoose/user');
+
 router.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false ,
@@ -13,4 +15,18 @@ router.use(session({
 
 router.use(passport.initialize()) 
 router.use(passport.session())    
+
+authUser = async (user, password, done) => {
+  //Search the user, password in the DB to authenticate the user
+  User.findOne({username: user}, (err, user) => {
+    if(err) return done(err);
+ 
+    if(!user) return done(null, false);
+
+    if(user.password != password) return done(null, false);
+
+    return done(null, user)
+  })
+}
+
 passport.use(new LocalStrategy (authUser))
