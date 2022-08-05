7762d652c9092655378d1aba529d35add4c94fbc
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-05 12:52:55 -0400

    required in dependencies

---
+++ server/routers/auth.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+const passport = require('passport')
+const session = require('express-session')
+const LocalStrategy = require('passport-local').Strategy
