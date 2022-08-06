2a9481afc27a653f8e24c227c731ff8ac1adf9d4
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-06 14:34:05 -0400

    took the auth routes out of server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ require('dotenv').config({path: __dirname + '/.env'});
 const users = require('./routers/users');
 const teams = require('./routers/teams')
 const logs = require('./routers/logs');
-const auth = require('./routers/auth');
 
 //.env imports
 const connectUsername = process.env.USERNAME;
@@ -30,7 +29,6 @@ app.use(express.json())
 app.use('/users', users);
 app.use('/teams', teams);
 app.use('/logs', logs);
-app.use('/auth', auth);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`)
