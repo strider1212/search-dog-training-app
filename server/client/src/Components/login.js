5c6ec80077d8309a7b19e5be44b0caa8c7e2690b
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-08 10:28:02 -0400

    in login.js, changed for to htmlFor to meet JSX syntax

--- server/client/src/Components/login.js
+++ server/client/src/Components/login.js
@@ -1,12 +1,16 @@
+import React, { useState } from 'react';
+
 export const Login = () => {
+  const [username, setUsername] = useState('');
+
   return (
     <form>
       <div>
-        <label for="username">Username:</label>
+        <label htmlFor="username">Username:</label>
         <input type="text" className="form-control" id="username" />
       </div>
       <div>
-        <label for="password">Password:</label>
+        <label htmlFor="password">Password:</label>
         <input type="password" className="form-control" id="password" />
       </div>
       <button type="button" className="btn btn-primary">Submit</button>
