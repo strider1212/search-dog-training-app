cbc78cadb150d590d54965e4bc31b06f41e73bba
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-08 10:02:32 -0400

    username input is up

--- server/client/src/routes/login.js
+++ server/client/src/routes/login.js
@@ -1,6 +1,10 @@
 export const Login = () => {
   return (
     <form>
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" className="form-control" id="username" />
+      </div>
       <button type="button" className="btn btn-primary">Submit</button>
     </form>
   )
