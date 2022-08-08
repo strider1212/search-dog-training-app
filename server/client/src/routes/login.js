ae74b69d2cc782607a8c7d1e1a6f87a846bc638e
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-08 10:03:27 -0400

    password input is up

--- server/client/src/routes/login.js
+++ server/client/src/routes/login.js
@@ -5,6 +5,10 @@ export const Login = () => {
         <label for="username">Username:</label>
         <input type="text" className="form-control" id="username" />
       </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" className="form-control" id="password" />
+      </div>
       <button type="button" className="btn btn-primary">Submit</button>
     </form>
   )
