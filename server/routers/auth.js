578d27d47ce8abc383e221588b50d0aa8cfc0f03
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-08 14:01:02 -0400

    set up express session

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const session = require('express-session');
+require('dotenv').config();
+
+router.use(session({ 
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
 router.get('/login', (req, res) => {
   console.log('testing GET /login')
