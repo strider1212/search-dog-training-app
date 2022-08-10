f21ac2a5d09feb956fcfbc2b2126986808f48082
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-10 18:43:50 -0400

    changed for to thtmlFor

--- server/client/src/Components/newLog.js
+++ server/client/src/Components/newLog.js
@@ -2,6 +2,101 @@ const NewLog = () => {
   return (
     <form>
       <div className="form-group">
+        <label htmlFor="created_by">Created by:</label>
+        <input type="text" className="form-control" id="created_by" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="date">Date:</label>
+        <input type="date" className="form-control" id="date" placeholder="Date on which the drill was executed..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="time">Time:</label>
+        <input type="time" className="form-control" id="time" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="address">Address:</label>
+        <input type="text" className="form-control" id="address" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="team">Team:</label>
+        <input type="text" className="form-control" id="team" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="training-type">Training Type:</label>
+        <input type="text" className="form-control" id="training-type" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="training-hours">Training Hours:</label>
+        <input type="number" className="form-control" id="training-hours" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="travel-hours">Travel Hours:</label>
+        <input type="number" className="form-control" id="travel-hours" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="aggregiate-hours">Aggregiate Hours:</label>
+        <input type="number" className="form-control" id="aggregiate-hours" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="number">Mileage:</label>
+        <input type="text" className="form-control" id="mileage" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="tolls">Tolls:</label>
+        <input type="number" className="form-control" id="tolls" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="time-of-day">Time of Day:</label>
+        <input type="text" className="form-control" id="time-of-day" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="weather">Weather:</label>
+        <input type="text" className="form-control" id="weather" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="tempterature">Temperature:</label>
+        <input type="number" className="form-control" id="tempterature" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="wind-speed">Wind Speed:</label>
+        <input type="number" className="form-control" id="wind-speed" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="humidity">Humidity:</label>
+        <input type="number" className="form-control" id="humidity" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="placement-description">Placement Description:</label>
+        <input type="text" className="form-control" id="placement-description" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="placed-by">Placed by:</label>
+        <input type="text" className="form-control" id="placed-by" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="scent-source">Scent Source:</label>
+        <input type="text" className="form-control" id="scent-source" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="source-container">Source Container:</label>
+        <input type="text" className="form-control" id="source-container" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label htmlFor="water">Water:</label>
+        <input type="checkbox" className="form-control" id="water" placeholder="Your Name..." />
+      </div>
+    </form>
+  )
+   
+     
+}
+    
+    
+
+export default NewLog;
+
+ <form>
+      <div className="form-group">
         <label for="created_by">Created by:</label>
         <input type="text" className="form-control" id="created_by" placeholder="Your Name..." />
       </div>
@@ -86,7 +181,3 @@ const NewLog = () => {
         <input type="checkbox" className="form-control" id="water" placeholder="Your Name..." />
       </div>
     </form>
-  )
-}
-
-export default NewLog;
