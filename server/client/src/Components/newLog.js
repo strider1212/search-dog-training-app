e26b353c123d482b5f4ff05059de6cc9fd1dd93c
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-10 17:31:16 -0400

    form for newLog fill out with dummy data to be adapted

--- server/client/src/Components/newLog.js
+++ server/client/src/Components/newLog.js
@@ -1,8 +1,91 @@
 const NewLog = () => {
   return (
-    <div>
-      NewLog.js
-    </div>
+    <form>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="date">Date:</label>
+        <input type="text" className="form-control" id="date" placeholder="Date on which the drill was executed..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Time:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+      <div className="form-group">
+        <label for="created_by">Created by:</label>
+        <input type="text" className="form-control" id="form-group" placeholder="Your Name..." />
+      </div>
+    </form>
   )
 }
 
