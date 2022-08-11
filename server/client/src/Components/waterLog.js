2293e0d4323f10080a659a8f37ed74d5d5cc6013
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-11 13:41:36 -0400

    set up comments for waterLog.js

--- server/client/src/Components/waterLog.js
+++ server/client/src/Components/waterLog.js
@@ -1,7 +1,17 @@
 import { formPopulater } from "../utils/formPopulater";
 
 const WaterLog = () => {
-  
+  //state
+
+  //formPopulaterArray
+
+  //forMapper
+
+  //submitHandler
+    //consts
+    //axios
+      //values
+      //Navigate
   
   return (
     <form>
