d0c85607303fc2c73502974c5d914229e80e930e
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-11 07:56:47 -0400

    refactored createdBy and Date to fit into formMapper

--- server/client/src/Components/newLog.js
+++ server/client/src/Components/newLog.js
@@ -24,234 +24,253 @@ const NewLog = () => {
   
 
   const submitHandler = async () => {
-    console.log(souceContainer)
+    console.log(createdBy)
     // await axios.post('/logs')
     // .then(res => console.log(res))
   }
 
+
+  const formPopulater = (forAndId, UIText, type, className, placeholder, setFunction) => {
+
+    return (
+      <div className="form-group">
+        <label htmlFor={forAndId}>{UIText}:</label>
+        <input 
+        type={type} 
+        className={className} 
+        id={forAndId} 
+        placeholder={placeholder}
+        onInput={(e) => setFunction(e.target.value)}
+        />
+      </div>
+    )
+  }
+
+  const formPopulaterArray = 
+  [formPopulater('created_by', 'Created By', 'text', 'form-control', 'Your Name...', setCreatedBy), 
+  formPopulater('"form-group"', 'Date', 'date', 'form-control', 'Date on which the drill was executed...', setDate)
+];
+
+  const formMapper = formPopulaterArray.map(func => func)
+
   return (
     <form>
-      <div className="form-group">
-        <label htmlFor="created_by">Created by:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="created_by" 
-        placeholder="Your Name..." 
-        onInput={(e) => setCreatedBy(e.target.valuej)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="date">Date:</label>
-        <input 
-        type="date" 
-        className="form-control" 
-        id="date" 
-        placeholder="Date on which the drill was executed..." 
-        onInput={(e) => setDate(e.target.value)} 
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="time">Time:</label>
-        <input 
-        type="time" 
-        className="form-control" 
-        id="time" 
-        onInput={(e) => setTime(e.target.value)} 
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="address">Address:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="address" 
-        placeholder="Address of the training..." 
-        onInput={(e) => setAddress(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="team">Team:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="team" 
-        placeholder="Team Name..." 
-        onInput={(e) => setTeam(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="training-type">Training Type:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="training-type" 
-        placeholder="Training type..." 
-        onInput={(e) => setTrainingType(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="training-hours">Training Hours:</label>
-        <input 
-        type="number" 
-        className="form-control" 
-        id="training-hours" 
-        placeholder="Enter a number (can use decimals)..." 
-        onInput={(e) => {setTrainingHours(e.target.value)}}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="travel-hours">Travel Hours:</label>
-        <input 
-        type="number" 
-        className="form-control" 
-        id="travel-hours" 
-        placeholder="Enter a number (can use decimals)..." 
-        onInput={(e) => setTravelHours(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="aggregiate-hours">Aggregiate Hours:</label>
-        <input 
-        type="number" 
-        className="form-control" 
-        id="aggregiate-hours" 
-        placeholder="Enter a number (can use decimals)..." 
-        onInput={(e) => setAggregiateHours(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="number">Mileage:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="mileage" 
-        placeholder="Enter a number for the number of miles driven..." 
-        onInput={(e) => setMileage(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="tolls">Tolls:</label>
-        <input 
-        type="number" 
-        className="form-control" 
-        id="tolls" 
-        placeholder="Enter a number..." 
-        onInput={(e) => setTolls(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="weather">Weather:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="weather" 
-        placeholder="Description of the weather..."
-        onInput={(e) => setWeather(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="tempterature">Temperature:</label>
-        <input 
-        type="number" 
-        className="form-control" 
-        id="tempterature" 
-        placeholder="Enter a number for degrees fahrenheit..." 
-        onInput={(e) => setTemperature(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="wind-speed">Wind Speed:</label>
-        <input 
-        type="number" 
-        className="form-control" 
-        id="wind-speed" 
-        placeholder="Enter a number for MPH..." 
-        onInput={(e) => setWindSpeed(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="humidity">Humidity:</label>
-        <input 
-        type="number" 
-        className="form-control" 
-        id="humidity" 
-        placeholder="Enter a number representing a percent..." 
-        onInput={(e) => setHumidity(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="placement-description">Placement Description:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="placement-description" 
-        placeholder="Placement Description..." 
-        onInput={(e) => setplacementDescription(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="placed-by">Placed by:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="placed-by" 
-        placeholder="Person who placed the source..." 
-        onInput={(e) => setPlacedBy(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="scent-source">Scent Source:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="scent-source" 
-        placeholder="Kind of source used..." 
-        onInput={(e) => setScentSource(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="source-container">Source Container:</label>
-        <input 
-        type="text" 
-        className="form-control" 
-        id="source-container" 
-        placeholder="What the source was in..." 
-        onInput={(e) => setsouceContainer(e.target.value)}
-        />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="water" className="form-check-label">Water:</label>
-        <input 
-        type="checkbox" 
-        className="form-check-input" 
-        id="water" 
-        placeholder="Your Name..." 
-        />
-      </div>
-
+      {formMapper}
       <button type='button' className='btn btn-primary' onClick={submitHandler}>Submit</button>
     </form>
-  )   
+  )
+  
+
+  //     <div className="form-group">
+  //       <label htmlFor="date">Date:</label>
+  //       <input 
+  //       type="date" 
+  //       className="form-control" 
+  //       id="date" 
+  //       placeholder="Date on which the drill was executed..." 
+  //       onInput={(e) => setDate(e.target.value)} 
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="time">Time:</label>
+  //       <input 
+  //       type="time" 
+  //       className="form-control" 
+  //       id="time" 
+  //       onInput={(e) => setTime(e.target.value)} 
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="address">Address:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="address" 
+  //       placeholder="Address of the training..." 
+  //       onInput={(e) => setAddress(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="team">Team:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="team" 
+  //       placeholder="Team Name..." 
+  //       onInput={(e) => setTeam(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="training-type">Training Type:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="training-type" 
+  //       placeholder="Training type..." 
+  //       onInput={(e) => setTrainingType(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="training-hours">Training Hours:</label>
+  //       <input 
+  //       type="number" 
+  //       className="form-control" 
+  //       id="training-hours" 
+  //       placeholder="Enter a number (can use decimals)..." 
+  //       onInput={(e) => {setTrainingHours(e.target.value)}}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="travel-hours">Travel Hours:</label>
+  //       <input 
+  //       type="number" 
+  //       className="form-control" 
+  //       id="travel-hours" 
+  //       placeholder="Enter a number (can use decimals)..." 
+  //       onInput={(e) => setTravelHours(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="aggregiate-hours">Aggregiate Hours:</label>
+  //       <input 
+  //       type="number" 
+  //       className="form-control" 
+  //       id="aggregiate-hours" 
+  //       placeholder="Enter a number (can use decimals)..." 
+  //       onInput={(e) => setAggregiateHours(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="number">Mileage:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="mileage" 
+  //       placeholder="Enter a number for the number of miles driven..." 
+  //       onInput={(e) => setMileage(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="tolls">Tolls:</label>
+  //       <input 
+  //       type="number" 
+  //       className="form-control" 
+  //       id="tolls" 
+  //       placeholder="Enter a number..." 
+  //       onInput={(e) => setTolls(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="weather">Weather:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="weather" 
+  //       placeholder="Description of the weather..."
+  //       onInput={(e) => setWeather(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="tempterature">Temperature:</label>
+  //       <input 
+  //       type="number" 
+  //       className="form-control" 
+  //       id="tempterature" 
+  //       placeholder="Enter a number for degrees fahrenheit..." 
+  //       onInput={(e) => setTemperature(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="wind-speed">Wind Speed:</label>
+  //       <input 
+  //       type="number" 
+  //       className="form-control" 
+  //       id="wind-speed" 
+  //       placeholder="Enter a number for MPH..." 
+  //       onInput={(e) => setWindSpeed(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="humidity">Humidity:</label>
+  //       <input 
+  //       type="number" 
+  //       className="form-control" 
+  //       id="humidity" 
+  //       placeholder="Enter a number representing a percent..." 
+  //       onInput={(e) => setHumidity(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="placement-description">Placement Description:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="placement-description" 
+  //       placeholder="Placement Description..." 
+  //       onInput={(e) => setplacementDescription(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="placed-by">Placed by:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="placed-by" 
+  //       placeholder="Person who placed the source..." 
+  //       onInput={(e) => setPlacedBy(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="scent-source">Scent Source:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="scent-source" 
+  //       placeholder="Kind of source used..." 
+  //       onInput={(e) => setScentSource(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="source-container">Source Container:</label>
+  //       <input 
+  //       type="text" 
+  //       className="form-control" 
+  //       id="source-container" 
+  //       placeholder="What the source was in..." 
+  //       onInput={(e) => setsouceContainer(e.target.value)}
+  //       />
+  //     </div>
+
+  //     <div className="form-group">
+  //       <label htmlFor="water" className="form-check-label">Water:</label>
+  //       <input 
+  //       type="checkbox" 
+  //       className="form-check-input" 
+  //       id="water" 
+  //       placeholder="Your Name..." 
+  //       />
+  //     </div>
+
+  //     <button type='button' className='btn btn-primary' onClick={submitHandler}>Submit</button>
+  //   </form>
+  // )   
 }
     
 export default NewLog;
