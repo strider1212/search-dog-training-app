8ffd5cba5c39521a8c0921596de3155dc4229812
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-12 10:01:23 -0400

    training hours

--- server/client/src/Components/newLog.js
+++ server/client/src/Components/newLog.js
@@ -92,8 +92,8 @@ const NewLog = () => {
       {formPopulater('address', 'Address', 'text', '', setFormValue, formValues, 'address')}
       {formPopulater('team', 'Team', 'text', '', setFormValue, formValues, 'team')}
       {formPopulater('training-type', 'Training Type', 'text', '', setFormValue, formValues, 'trainingType')}
+      {formPopulater('training-hours', 'Training Hours', 'number', '', setFormValue, formValues, 'trainingHours')}
       {/* 
-      {formPopulater('training-type', 'Training Type', 'text', 'form-control', 'Training type...', setTrainingType)}
       {formPopulater('training-hours', 'Training Hours', 'number', 'form-control', 'Enter a number (can use decimals)...', setTrainingHours)}
       {formPopulater('travel-hours', 'Travel Hours', 'number', 'form-control', 'Enter a number (can use decimals)...', setTravelHours)}
       {formPopulater('aggregiate-hours', 'Aggregiate Hours', 'number', 'form-control', 'Enter a number (can use decimals)...', setAggregiateHours)}
