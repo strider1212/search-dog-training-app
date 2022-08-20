d6b2722d5f218dac4ead172fd1af6940f57a4e61
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-20 17:18:58 -0400

    required in apiKey through environment variable

--- server/utils/geoCoder.js
+++ server/utils/geoCoder.js
@@ -1,5 +1,9 @@
-const geoCoder = () => {
+const {Client} = require("@googlemaps/google-maps-services-js");
+require('dotenv').config()
 
+const geoCoder = () => {
+  const apikey = process.env.GOOGLE_MAPS_API_KEY;
+  const client = new Client({});
 }
 
 module.exports = geoCoder;
