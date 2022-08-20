3d0b9d8184f23177e89e8fc4301aa2d75a73b4c6
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-20 18:01:05 -0400

    returns an object with lat and long

--- server/utils/geoCoder.js
+++ server/utils/geoCoder.js
@@ -1,14 +1,22 @@
-const {Client} = require("@googlemaps/google-maps-services-js");
 require('dotenv').config()
 const axios = require('axios');
 
 const geoCoder = async () => {
   const apikey = process.env.GOOGLE_MAPS_API_KEY;
+
+  let lat = '';
+  let lng = '';
+
   await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=196+Green+Giant+Rd,+Townsend,+DE&key=${apikey}`)
   .then(res => {
-    console.log(res.data.results[0].geometry.location)
+    console.log(res.data.results[0].geometry.location.lat)
+    lat = res.data.results[0].geometry.location.lat;
+    long = res.data.results[0].geometry.location.lng;
   })
-
+  return {
+    lat: lat,
+    lng: lng
+  }
 }
 
 geoCoder()
