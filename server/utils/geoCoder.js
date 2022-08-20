c4335be10dfc4d2b1f8370fedc031b6a1e310d74
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-20 17:57:45 -0400

    I can now reach long and lat through the api

--- server/utils/geoCoder.js
+++ server/utils/geoCoder.js
@@ -1,9 +1,16 @@
 const {Client} = require("@googlemaps/google-maps-services-js");
 require('dotenv').config()
+const axios = require('axios');
 
-const geoCoder = () => {
+const geoCoder = async () => {
   const apikey = process.env.GOOGLE_MAPS_API_KEY;
-  const client = new Client({});
+  await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=196+Green+Giant+Rd,+Townsend,+DE&key=${apikey}`)
+  .then(res => {
+    console.log(res.data.results[0].geometry.location)
+  })
+
 }
+
+geoCoder()
 
 module.exports = geoCoder;
