2b212555661682e3ef52ecab7ff97b12baf63a7f
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-26 06:47:46 -0400

    first name input added

--- server/client/src/Components/signUp.js
+++ server/client/src/Components/signUp.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const SignUp = () => {
-  const [usename, setUsername] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
 
   const navigate = useNavigate();
 
   const postHandler = () => {
     console.log('Pokemon')
-    return 'Pokemon'
+    // await axios.post('/users')
   }
   
   return (
@@ -18,8 +20,13 @@ const SignUp = () => {
       <form>
         <label htmlFor='username'>Username:</label>
         <input type='text' className="form-control" id='username' placeholder="username" onInput={(e) => setUsername(e.target.value)}/>
+
         <label htmlFor='password'>Username:</label>
         <input type='password' className="form-control" id='password' placeholder="password" onInput={(e) => setPassword(e.target.value)}/>
+
+        <label htmlFor='first-name'>First Name:</label>
+        <input type='text' className="form-control" id='first-name' placeholder="First Name" onInput={(e) => setFirstName(e.target.value)}/>
+
 
         <button type='button' className='btn btn-primary' onClick={() => postHandler()}>Submit</button>
         <button type='button' className='btn btn-secondary' onClick={() => navigate('/')}>Cancel</button>
