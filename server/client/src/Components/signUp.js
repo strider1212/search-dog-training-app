5862c18eb257cbe8b755fcda39ea357867ae0ea9
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-26 06:30:36 -0400

    username input in sign up

--- server/client/src/Components/signUp.js
+++ server/client/src/Components/signUp.js
@@ -1,6 +1,11 @@
 const SignUp = () => {
   return (
-    <div>signUp.js</div>
+    <body>
+      <form>
+        <label htmlFor='username'>Username:</label>
+        <input type='text' className="form-control" id='username' placeholder="username" />
+      </form>
+    </body>
   )
 }
 
