8a8c9c6d06d2239aeabca3fc94d19e222fa1f2c6
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-26 06:32:02 -0400

    ibid. for password

--- server/client/src/Components/signUp.js
+++ server/client/src/Components/signUp.js
@@ -5,6 +5,8 @@ const SignUp = () => {
       <form>
         <label htmlFor='username'>Username:</label>
         <input type='text' className="form-control" id='username' placeholder="username" />
+        <label htmlFor='password'>Username:</label>
+        <input type='password' className="form-control" id='password' placeholder="password" />
       </form>
     </body>
   )
