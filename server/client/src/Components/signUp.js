a159b3d4c5e4f073409b04fca006d4fd16f7befd
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-08-26 06:39:34 -0400

    postHandler is connected

--- server/client/src/Components/signUp.js
+++ server/client/src/Components/signUp.js
@@ -6,6 +6,11 @@ const SignUp = () => {
   const [password, setPassword] = useState('');
 
   const navigate = useNavigate();
+
+  const postHandler = () => {
+    console.log('Pokemon')
+    return 'Pokemon'
+  }
   
   return (
     <body>
@@ -16,7 +21,7 @@ const SignUp = () => {
         <label htmlFor='password'>Username:</label>
         <input type='password' className="form-control" id='password' placeholder="password" onInput={(e) => setPassword(e.target.value)}/>
 
-        <button type='button' className='btn btn-primary'>Submit</button>
+        <button type='button' className='btn btn-primary' onClick={() => postHandler()}>Submit</button>
         <button type='button' className='btn btn-secondary' onClick={() => navigate('/')}>Cancel</button>
       </form>
     </body>
