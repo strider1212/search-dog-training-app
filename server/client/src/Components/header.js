58472926d3eb4e144ed3b9ecf1a893f812b15a25
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-10-26 08:15:25 -0400

    made DogLog a navbar-brand

--- server/client/src/Components/header.js
+++ server/client/src/Components/header.js
@@ -3,9 +3,9 @@ import { useNavigate } from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <nav className="navbar">
+    <nav className="navbar navbar-expand-sm">
       <ul className="navbar-nav ml-auto">
-        <li className="nav-item"><div>DogLog</div></li>
+        <li className="navbar-brand"><div>DogLog</div></li>
         <li className="nav-item"><button type="button" className="btn btn-primary" onClick={() => navigate('/signUp')}>Sign up</button></li>
         <li className="nav-item"><button type="button" className="btn btn-primary" onClick={() => navigate('/signIn')}>Sign in</button></li>
       </ul>
