09da9d71245bc080f014bc73087f30a1971285b2
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-10-26 08:19:52 -0400

    made sign up and sign in nav-links

--- server/client/src/Components/header.js
+++ server/client/src/Components/header.js
@@ -6,8 +6,8 @@ const Header = () => {
     <nav className="navbar navbar-expand-sm">
       <ul className="navbar-nav ml-auto">
         <li className="navbar-brand"><div>DogLog</div></li>
-        <li className="nav-item"><button type="button" className="btn btn-primary" onClick={() => navigate('/signUp')}>Sign up</button></li>
-        <li className="nav-item"><button type="button" className="btn btn-primary" onClick={() => navigate('/signIn')}>Sign in</button></li>
+        <li className="nav-item"><button type="button" className="btn btn-primary nav-link" onClick={() => navigate('/signUp')}>Sign up</button></li>
+        <li className="nav-item"><button type="button" className="btn btn-primary nav-link" onClick={() => navigate('/signIn')}>Sign in</button></li>
       </ul>
     </nav>
   )
