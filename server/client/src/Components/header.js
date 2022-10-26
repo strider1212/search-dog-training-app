27179962136f2ab9daceddff155232d63c9a39b1
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-10-26 08:07:20 -0400

    populated the navbar with list items

--- server/client/src/Components/header.js
+++ server/client/src/Components/header.js
@@ -3,10 +3,12 @@ import { useNavigate } from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div>DogLog</div>
-      <button type="button" className="btn btn-primary" onClick={() => navigate('/signUp')}>Sign up</button>
-      <button type="button" className="btn btn-primary" onClick={() => navigate('/signIn')}>Sign in</button>
+    <nav className="navbar">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item"><div>DogLog</div></li>
+        <li className="nav-item"><button type="button" className="btn btn-primary" onClick={() => navigate('/signUp')}>Sign up</button></li>
+        <li className="nav-item"><button type="button" className="btn btn-primary" onClick={() => navigate('/signIn')}>Sign in</button></li>
+      </ul>
     </nav>
   )
 }
