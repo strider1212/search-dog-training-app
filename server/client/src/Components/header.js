d24128ca032e89efe719cd9b8a6b41e0775da0c8
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-10-26 08:25:49 -0400

    took away .btn top margins

--- server/client/src/Components/header.js
+++ server/client/src/Components/header.js
@@ -4,8 +4,8 @@ const Header = () => {
   const navigate = useNavigate();
   return (
     <nav className="navbar navbar-expand-sm">
-      <ul className="navbar-nav ml-auto">
-        <li className="navbar-brand"><div>DogLog</div></li>
+      <div className="navbar-brand">DogLog</div>
+      <ul className="navbar-nav ml-auto navbar-togglers">
         <li className="nav-item"><button type="button" className="btn btn-primary nav-link" onClick={() => navigate('/signUp')}>Sign up</button></li>
         <li className="nav-item"><button type="button" className="btn btn-primary nav-link" onClick={() => navigate('/signIn')}>Sign in</button></li>
       </ul>
