a144954c99fa172591fb971478f474c6ba6b2c38
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-10-26 08:28:14 -0400

    created home-margin class and applied it to buttons in the center of the home page, rather than making the top-margin part of the .btn class itself

--- server/client/src/Components/home.js
+++ server/client/src/Components/home.js
@@ -11,14 +11,14 @@ const Home = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg">
-          <div className="row">
+          <div className="row home-margin">
             <div className="col">
               <Link to="/newLog">
                 <button type="button" className="btn btn-primary">New Log</button>
               </Link>
             </div>
           </div>
-          <div className="row">
+          <div className="row home-margin">
             <div className="col">
               <button type="button" className="btn btn-primary">Edit Logs</button>
             </div>
