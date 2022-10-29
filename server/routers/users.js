925f8587d85e6b60341b635176e58bbcc814fd0c
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-10-29 09:21:45 -0400

    required in jwt simple

--- server/routers/users.js
+++ server/routers/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const LocalStrategy = require("passport-local").Strategy;
+const jwt = require("jwt-simple");
 require('dotenv').config('.env');
 
 const connectUsername = process.env.USERNAME;
