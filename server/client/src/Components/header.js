ac885652af78a54ea10383ac3e1ac9fdedaad793
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-11-11 15:47:31 -0500

    alert that tells the user if they signed out

--- server/client/src/Components/header.js
+++ server/client/src/Components/header.js
@@ -2,13 +2,17 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
+
   return (
     <nav className="navbar navbar-expand-sm">
       <div className="navbar-brand">DogLog</div>
       <ul className="navbar-nav">
         <li className="nav-item"><button type="button" className="btn btn-primary nav-link" onClick={() => navigate('/signUp')}>Sign up</button></li>
         <li className="nav-item"><button type="button" className="btn btn-primary nav-link" onClick={() => navigate('/signIn')}>Sign in</button></li>
-        <li className="nav-item"><button type="button" className="btn btn-secondary nav-link" onClick={() => localStorage.clear()}>Sign out</button></li>
+        <li className="nav-item"><button type="button" className="btn btn-secondary nav-link" onClick={() => {
+          localStorage.clear()
+          alert('User successfully signed out')
+        }}>Sign out</button></li>
       </ul>        
     </nav>
   )
