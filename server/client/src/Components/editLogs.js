279ad97581eadaba47e8a9a7fc6b86988f448127
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-12 06:28:31 -0500

    testing array.map() works

--- server/client/src/Components/editLogs.js
+++ server/client/src/Components/editLogs.js
@@ -2,14 +2,19 @@ import React, {useEffect} from 'react';
 
 const EditLogs = () => {
 
-  const testingUseEffect = () => {
-    console.log('testing useEffect')
-  }
+  // const testingUseEffect = () => {
+  //   console.log('testing useEffect')
+  // }
 
-  useEffect(() => testingUseEffect())
+  // useEffect(() => testingUseEffect())
+  const arrayOne = [1, 2, 3, 4]
+
+  const mapTest = arrayOne.map(i => {
+    return `the next number is ${i}`
+  })
   
   return (
-    <p>testing</p>
+    <p>{mapTest}</p>
   )
 }
 
