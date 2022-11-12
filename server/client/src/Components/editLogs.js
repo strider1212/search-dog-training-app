c77de0ea5e53dce13937f3c1b2ac82df12f58c54
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-12 06:31:39 -0500

    created editLogsDisplayGrid

--- server/client/src/Components/editLogs.js
+++ server/client/src/Components/editLogs.js
@@ -2,19 +2,23 @@ import React, {useEffect} from 'react';
 
 const EditLogs = () => {
 
+  const editLogsDisplayGrid = () => {
+    return 'testing editLogdsDisplayGrid'
+  }
   // const testingUseEffect = () => {
   //   console.log('testing useEffect')
   // }
 
   // useEffect(() => testingUseEffect())
-  const arrayOne = [1, 2, 3, 4]
 
-  const mapTest = arrayOne.map(i => {
-    return `the next number is ${i}`
-  })
+  // const arrayOne = [1, 2, 3, 4]
+
+  // const mapTest = arrayOne.map(i => {
+  //   return `the next number is ${i}`
+  // })
   
   return (
-    <p>{mapTest}</p>
+    <p>{editLogsDisplayGrid()}</p>
   )
 }
 
