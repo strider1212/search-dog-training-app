b7dba2a6873bdc5a8b5dd25b75298225003fa3ae
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-12 17:23:15 -0500

    commented all code in editLogs.js

--- server/client/src/Components/editLogs.js
+++ server/client/src/Components/editLogs.js
@@ -3,37 +3,43 @@ import axios from 'axios';
 
 const EditLogs = () => {
 
-  const arrayOfLogs = []
+  // const arrayOfLogs = []
 
-  const arrayOfLogDisplayed = arrayOfLogs.map((e, i) => {
-    return `Number ${i} is ${e}`
-  })
-  
-  const editLogsDisplayGrid = () => {
-    console.log(arrayOfLogDisplayed.length)
-    if (arrayOfLogDisplayed.length === 0) {
-      return 'No logs loaded'
-    } else {
-      return arrayOfLogDisplayed;
-    }
-  }
-  // const testingUseEffect = () => {
-  //   console.log('testing useEffect')
-  // }
-
-  useEffect(() => {
-    axios.get('http://localhost:3000/logs')
-    .then(res => console.log(res))
-  })
-
-  // const arrayOne = [1, 2, 3, 4]
-
-  // const mapTest = arrayOne.map(i => {
-  //   return `the next number is ${i}`
+  // const arrayOfLogDisplayed = arrayOfLogs.map((e, i) => {
+  //   return `<p>Number ${i} is ${e}</p>`
   // })
   
+  // const editLogsDisplayGrid = () => {
+  //   // if (arrayOfLogDisplayed.length === 0) {
+  //   //   return 'No logs loaded'
+  //   // } else {
+  //   //   return arrayOfLogDisplayed;
+  //   // }
+  //   return 'test'
+  // }
+  // // const testingUseEffect = () => {
+  // //   console.log('testing useEffect')
+  // // }
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:3000/logs')
+  //   .then(res => {
+  //     console.log('result from the get request to all logs', res)
+  //     console.log('res.data array:', res.data)
+  //     res.data.map(e => arrayOfLogs.push(e))
+  //   })
+  // })
+
+  // // const arrayOne = [1, 2, 3, 4]
+
+  // // const mapTest = arrayOne.map(i => {
+  // //   return `the next number is ${i}`
+  // // })
+  
   return (
-    <p>{editLogsDisplayGrid()}</p>
+    <div>
+      <p>editLogs.js</p>
+    </div>
   )
 }
 
