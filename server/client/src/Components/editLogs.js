95f62c3d41e285ee79b282a08e4aa0db13dc5fae
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-12 06:23:54 -0500

    useEffect works on editLogs.js

--- server/client/src/Components/editLogs.js
+++ server/client/src/Components/editLogs.js
@@ -1,5 +1,12 @@
+import React, {useEffect} from 'react';
 
 const EditLogs = () => {
+
+  const testingUseEffect = () => {
+    console.log('testing useEffect')
+  }
+
+  useEffect(() => testingUseEffect())
   
   return (
     <p>testing</p>
