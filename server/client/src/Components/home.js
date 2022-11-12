9481fbf09a778bb2f2be473982b3af7edc5e065e
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-12 17:05:18 -0500

    did some organizational refactoring in home.js

--- server/client/src/Components/home.js
+++ server/client/src/Components/home.js
@@ -1,7 +1,8 @@
-import { useNavigate } from "react-router-dom";
 import Header from "../Components/header";
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
+//utils
 import { HeaderInsert } from '../utils/headerInsert';
 import { AuthorizationAlert } from '../utils/authorizationAlert';
 
