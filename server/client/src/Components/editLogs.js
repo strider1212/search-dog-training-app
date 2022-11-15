8e77535a20fb8045164fef501fa2a6d17c8541bc
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-14 20:16:39 -0500

    can push all of the logs to an array

--- server/client/src/Components/editLogs.js
+++ server/client/src/Components/editLogs.js
@@ -3,9 +3,17 @@ import axios from 'axios';
 
 const EditLogs = () => {
   
+  let holder = []
+  
   useEffect(() => {
     axios.get('http://localhost:3000/logs/')
-    .then(res => console.log(res))
+    .then(res => {
+      const pusher = res.data
+      pusher.map((e) => {
+        holder.push(e)
+      })
+    })
+    .then(console.log('holder:', holder))
   })
 
   return (
