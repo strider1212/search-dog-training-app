7c3a1c922f7ec47fcfa671760c663380834f8cdf
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-22 09:27:41 -0500

    created src/Redux/store.js

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
