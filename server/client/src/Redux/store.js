bf1dcab945ea90971bd6d1bacbc22c56a36455c8
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-22 09:55:49 -0500

    import currentUserToState into store.js

--- server/client/src/Redux/store.js
+++ server/client/src/Redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { currentUserToState } from './actions'
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    currentUser: currentUserToState
+  },
 })
