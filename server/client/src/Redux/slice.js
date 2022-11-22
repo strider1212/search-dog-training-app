620980eb6a7d68630b06d96f90dc3604c751ea00
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-22 10:36:07 -0500

    last commit before starting over with redux how i know how

---
+++ server/client/src/Redux/slice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const usernameToStateSlice = createSlice({
+  name: 'usernameToState',
+  initialState: '',
+  reducers: {
+    usernameToStateReducer: () => {
+
+    }
+  }
+})
