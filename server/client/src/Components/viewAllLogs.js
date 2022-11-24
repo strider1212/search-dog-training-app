c76adc9da37219f42974fd642b3c6ade7f2dc694
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-24 10:53:14 -0500

    afformentioned Go Back button returns user to home.js

--- server/client/src/Components/viewAllLogs.js
+++ server/client/src/Components/viewAllLogs.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 const ViewAllLogs = () => {
+  const navigate = useNavigate()
+
   return (
-    <button type="button" className="btn btn-secondary">Go Back</button>
+    <button type="button" className="btn btn-secondary" onClick={() => navigate('/')}>Go Back</button>
   )
 }
 
