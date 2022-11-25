8db8c0dd4657d1519c8158b23ffe80c3803defc9
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-25 08:14:57 -0500

    entered in training_info

--- server/client/src/Components/viewIndividualLog.js
+++ server/client/src/Components/viewIndividualLog.js
@@ -54,6 +54,13 @@ const ViewIndividualLog = () => {
       k9: '',
       notes: '',
       times: ''
+    },
+    training_info: {
+      placed_by: '',
+      placement_description: '',
+      scent_source: '',
+      source_container: '',
+      training_type: ''
     }
   }
   
@@ -85,7 +92,22 @@ const ViewIndividualLog = () => {
           notes: res.data.individual_runs.notes,
           // times
           times: res.data.individual_runs.times
+        },
+        training_info: {
+          placed_by: res.data.training_info.placed_by,
+          placement_description: res.data.training_info.placement_description,
+          scent_source: res.data.training_info.scent_source,
+          source_container: res.data.training_info.source_container,
+          training_type: res.data.training_info.training_type
         }
+
+
+        // training_info
+          // placed_by
+          // placement_description
+          // scent_source
+          // source_container
+          // training_type
       })
     })
   })
