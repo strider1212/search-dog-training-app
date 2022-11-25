39444217623761e49af883f27e56f8676924dddf
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-25 07:42:00 -0500

    created notes of categories to fill in viewIndividualLog.js

--- server/client/src/Components/viewIndividualLog.js
+++ server/client/src/Components/viewIndividualLog.js
@@ -5,17 +5,49 @@ const ViewIndividualLog = () => {
 
   const standInLogId = '637f502fe5a2feca81424f15'
 
-  // const initialState = {
-  //   address: ''
-  // }
-  
-  // const [renderValues, setRenderValues] = useState(initialState)
+  /*
+    date
+    log_created_by
+    team
+    time
+    hours and stats
+      mileage
+      tolls
+      total_hours
+      training_hours
+      travel_hours
+    individual_runs
+      blind
+      distractions
+      k9
+      notes
+      times
+    training_info
+      placed_by
+      placement_description
+      scent_source
+      source_container
+      training_type
+    water
+      depth
+      open
+      salt_water
+      submerged
+      temperature
+      water_type
+    weather
+      humidity
+      temperature
+      weather
+      wind_speed
+  */
 
   const [addressState, setAddressState] = useState('')
 
   useEffect(() => {
     axios.get(`http://localhost:3000/logs/${standInLogId}`)
     .then(res => {
+      console.log(res.data)
       return setAddressState(res.data.address)
     })
   })
