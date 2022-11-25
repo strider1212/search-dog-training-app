11db6b61adcee138d88d0205bd754401538a6d09
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-25 18:10:11 -0500

    refactored Training Informatino

--- server/client/src/Components/viewIndividualLog.js
+++ server/client/src/Components/viewIndividualLog.js
@@ -161,41 +161,18 @@ const ViewIndividualLog = () => {
                 )}
               </div>
             {RenderHeader('Training Information')}
-            <div className="row">
-              <div className="col">
-                <div className="row">
-                  <div className="col view-individual-log-main">
-                    Training Type: {individualLogValues.training_info.training_type}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col view-individual-log-main">
-                    Placed by: {individualLogValues.training_info.placed_by}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col view-individual-log-main">
-                    Scent Source: {individualLogValues.training_info.scent_source}
-                  </div>
-                </div>
+              <div className="row">
+                {RenderColumn(
+                  RenderIndividualInfo('Training Type: ', individualLogValues.training_info.training_type),
+                  RenderIndividualInfo('Placed by: ', individualLogValues.training_info.placed_by),
+                  RenderIndividualInfo('Scent Source: ', individualLogValues.training_info.scent_source)
+                )}
+                {RenderColumn(
+                  RenderIndividualInfo(),
+                  RenderIndividualInfo('Placement Description: ', individualLogValues.training_info.placement_description),
+                  RenderIndividualInfo('Source Container: ', individualLogValues.training_info.source_container)
+                )}
               </div>
-              <div className="col">
-                <div className="row">
-                  <div className="col view-individual-log-main">
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col view-individual-log-main">
-                    Placement Description: {individualLogValues.training_info.placement_description}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col view-individual-log-main">
-                    Source Container: {individualLogValues.training_info.source_container}
-                  </div>
-                </div>
-              </div>
-            </div>
             {RenderHeader('Water Information')}
             <div className="row">
               <div className="col">
