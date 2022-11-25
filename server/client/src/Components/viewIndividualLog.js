ffba4827e2f1d1a402fd933bcd246558a25eaaa5
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-25 06:37:17 -0500

    useEffect in viewIndividualLog.js does a basic axios.get() request to the log of the stand in ID

--- server/client/src/Components/viewIndividualLog.js
+++ server/client/src/Components/viewIndividualLog.js
@@ -1,4 +1,15 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+
 const ViewIndividualLog = () => {
+
+  const standInLogId = '637f502fe5a2feca81424f15'
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/logs/${standInLogId}`)
+    .then(res => console.log(res.data))
+  })
+
   return (
     <div style={{color: 'white'}}>View Inidividual Logs here</div>
   )
