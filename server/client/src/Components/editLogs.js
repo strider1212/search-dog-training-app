1184f6cb912960834ff7fa5dfba110ac55a1bc6d
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-11-30 05:41:46 -0500

    made it so that the delete button does not automatically go to the page by creating a stateholder

--- server/client/src/Components/editLogs.js
+++ server/client/src/Components/editLogs.js
@@ -28,40 +28,44 @@ const EditLogs = () => {
 
 
   const listLogs = logsState.map((log, index) => {
-    const logId = log._id
-    const readableId = logId.slice(logId.length - 4, logId.length)
-    const formattedDate = new Date(log.date);
-    const formattedMonth = formattedDate.getMonth() + 1
-    const formattedDay = formattedDate.getDate()
+      const logId = log._id
+      const readableId = logId.slice(logId.length - 4, logId.length)
+      const formattedDate = new Date(log.date);
+      const formattedMonth = formattedDate.getMonth() + 1
+      const formattedDay = formattedDate.getDate()
+      
+      let stateHolder = 0;
 
-
-    const deleteSelectedLog = (index) => {
-      const logId = logsState[index]._id
-      const confirmation = window.confirm(`You are about to delete Log #${logId}. Are you sure that you want to do that?`)
-      if (confirmation) {
-        axios.delete(`http://localhost:3000/logs/${logId}`)
-        .then(() => setLogsState([
-          ...logsState.slice(0, index),
-          ...logsState.slice(index + 1, logsState.length)
-        ]))
+      const deleteSelectedLog = (index) => {
+        stateHolder += 1;
+        const logId = logsState[index]._id
+        const confirmation = window.confirm(`You are about to delete Log #${logId}. Are you sure that you want to do that?`)
+        if (confirmation) {
+          axios.delete(`http://localhost:3000/logs/${logId}`)
+          .then(() => setLogsState([
+            ...logsState.slice(0, index),
+            ...logsState.slice(index + 1, logsState.length)
+          ]))
+        }
       }
-    }
-  
-    const navigateToIndividualLog = (Id) => {
-      navigate('/viewIndividualLog', {state: {_id: Id}})
-    }
     
-    return (
-      <li 
-        key={index} 
-        className="list-group-item list-group-item-action light-grey-background text-white"
-        onClick={() => navigateToIndividualLog(logId)}
-        >
-          <div>{`ID#: ${readableId}`}</div>
-          <div>{`${formattedMonth}/${formattedDay}/${formattedDate.getFullYear()}: ${log.address}`}</div>
-          <button type='button' className="btn btn-danger" onClick={() => deleteSelectedLog(index)}>Delete</button>
-      </li>
-    )
+      const navigateToIndividualLog = (Id) => {
+        if (stateHolder === 0) {
+          navigate('/viewIndividualLog', {state: {_id: Id}})
+        }
+      }
+      
+      return (
+        <li 
+          key={index} 
+          className="list-group-item list-group-item-action light-grey-background text-white"
+          onClick={() => navigateToIndividualLog(logId)}
+          >
+            <div>{`ID#: ${readableId}`}</div>
+            <div>{`${formattedMonth}/${formattedDay}/${formattedDate.getFullYear()}: ${log.address}`}</div>
+            <button type='button' className="btn btn-danger" onClick={() => deleteSelectedLog(index)}>Delete</button>
+        </li>
+      )
   })
 
 
