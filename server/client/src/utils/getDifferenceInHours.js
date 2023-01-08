0caee102c4cdd934a7fd70dd554cc4c09c4440d6
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2023-01-08 15:24:16 -0500

    removed console.logs that were for testing purposes

--- server/client/src/utils/getDifferenceInHours.js
+++ server/client/src/utils/getDifferenceInHours.js
@@ -1,14 +1,9 @@
 export const getDifferenceInHours = (date, time) => {
   const dateAndTime = `${date}T${time}`;
-  console.log('dateAndTime: ', dateAndTime)
   const dateAndTimeUTC = new Date(dateAndTime);
-  console.log('dateAndTimeUTC in getDifferenceInHours(): ', dateAndTimeUTC)
   const current = new Date()
-  console.log('current in getDifferenceInHours(): ', current)
   const difference = current - dateAndTimeUTC;
-  console.log('difference in getDifferenceInHours(): ', difference)
   const equation = -Math.round(difference/3600000)
-  console.log('equation in getDifferenceInHours(): ', equation)
 
   if (equation === -0) {
     return -1
